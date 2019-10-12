fcb4070a1eb5464f5cc3edf28c3b4ee3db980322
Mauricio Garcia <garmauco@gmail.com>
2019-10-11 19:57:22 -0500

    movil
    Desactivado el clic move

--- Build/Android/src/com/YourCompany/meat_thief/OBBData.java
+++ Build/Android/src/com/YourCompany/meat_thief/OBBData.java
@@ -19,7 +19,7 @@ public static final XAPKFile[] xAPKS = {
 new XAPKFile(
 true, // true signifies a main file
 "1", // the version of the APK that the file was uploaded against
-140289380L // the length of the file in bytes
+144600870L // the length of the file in bytes
 )
 };
 };
